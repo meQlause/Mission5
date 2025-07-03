970fb5607ff35ba6256aec2330595d990149993d
meQlause <me@ardial.tech>
2025-07-03 20:15:03 +0700

    feat: enhance product page layout with new content structure

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,11 +1,15 @@
 import { CategoryComponent } from "../components/category";
 import { CheckboxUI } from "../components/ui/checkbox";
+import { TextInput } from "../components/ui/input";
+import { StarRatingUi } from "../components/ui/stars";
 import { DefaultLayout } from "../layouts/default";
 import { FooterLayout } from "../layouts/footer";
 import { HeaderLayout } from "../layouts/header";
+import { useIsMobile } from "../utils/useIsMobile";
 
 export const ProductsPage = () => {
-  const content: Record<string, React.ReactNode> = {
+  const isMobile = useIsMobile();
+  const categoryContent: Record<string, React.ReactNode> = {
     "1": (
       <div className="ml-1 flex flex-row items-center justify-start gap-5">
         <CheckboxUI />
@@ -31,11 +35,92 @@ export const ProductsPage = () => {
       </div>
     ),
   };
+
+  const contents = [
+    {
+      id: 1,
+      contentImage: "assets/contents/content1.jpg",
+      title: "Big Auditor Financial Analyst",
+      description:
+        "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan kurikulum terbaik",
+      avatar: "assets/contents/avatar1.png",
+      name: "Jena Ortega",
+      position: "Senior accountant di Gojek",
+      rating: 3.5,
+      reviewCount: 86,
+      price: "Rp 300K",
+    },
+    {
+      id: 2,
+      contentImage: "assets/contents/content2.jpg",
+      title: "Big Auditor Financial Analyst",
+      description:
+        "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan kurikulum terbaik",
+      avatar: "assets/contents/avatar2.png",
+      name: "Jena Ortega",
+      position: "Senior accountant di Gojek",
+      rating: 3.5,
+      reviewCount: 86,
+      price: "Rp 300K",
+    },
+    {
+      id: 3,
+      contentImage: "assets/contents/content3.jpg",
+      title: "Big Auditor Financial Analyst",
+      description:
+        "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan kurikulum terbaik",
+      avatar: "assets/contents/avatar3.png",
+      name: "Jena Ortega",
+      position: "Senior accountant di Gojek",
+      rating: 3.5,
+      reviewCount: 86,
+      price: "Rp 300K",
+    },
+    {
+      id: 4,
+      contentImage: "assets/contents/content4.jpg",
+      title: "Big Auditor Financial Analyst",
+      description:
+        "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan kurikulum terbaik",
+      avatar: "assets/contents/avatar4.png",
+      name: "Jena Ortega",
+      position: "Senior accountant di Gojek",
+      rating: 3.5,
+      reviewCount: 86,
+      price: "Rp 300K",
+    },
+    {
+      id: 5,
+      contentImage: "assets/contents/content5.jpg",
+      title: "Big Auditor Financial Analyst",
+      description:
+        "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan kurikulum terbaik",
+      avatar: "assets/contents/avatar5.png",
+      name: "Jena Ortega",
+      position: "Senior accountant di Gojek",
+      rating: 3.5,
+      reviewCount: 86,
+      price: "Rp 300K",
+    },
+    {
+      id: 6,
+      contentImage: "assets/contents/content6.jpg",
+      title: "Big Auditor Financial Analyst",
+      description:
+        "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan kurikulum terbaik",
+      avatar: "assets/contents/avatar6.png",
+      name: "Jena Ortega",
+      position: "Senior accountant di Gojek",
+      rating: 3.5,
+      reviewCount: 86,
+      price: "Rp 300K",
+    },
+  ];
   return (
     <>
       <HeaderLayout />
-      <div className="px-standard flex flex-row gap-5 py-14">
-        <DefaultLayout className="w-full max-w-80 rounded-md p-5">
+      <div className="px-standard flex flex-col gap-5 py-14 md:flex-row">
+        <DefaultLayout className="h-fit w-full max-w-80 rounded-md p-5">
           <div className="mb-3 flex w-full flex-row items-center justify-between">
             <h6 className="text-heading6 font-bold">Filter</h6>
             <h6 className="text-heading6 font-bold text-red-500">Reset</h6>
@@ -51,7 +136,7 @@ export const ProductsPage = () => {
                   </div>
                 }
                 titleClassName="text-[#3ECF4C]"
-                content={content}
+                content={categoryContent}
                 contentClassName="text-[#3A3541AD] hover:text-[#3ECF4C] hover:text-decoration-none"
               />
             </div>
@@ -67,7 +152,7 @@ export const ProductsPage = () => {
                   </div>
                 }
                 titleClassName="text-[#3ECF4C]"
-                content={content}
+                content={categoryContent}
                 contentClassName="text-[#3A3541AD] hover:text-[#3ECF4C] hover:text-decoration-none"
               />
             </div>
@@ -83,12 +168,92 @@ export const ProductsPage = () => {
                   </div>
                 }
                 titleClassName="text-[#3ECF4C]"
-                content={content}
+                content={categoryContent}
                 contentClassName="text-[#3A3541AD] hover:text-[#3ECF4C] hover:text-decoration-none"
               />
             </div>
           </DefaultLayout>
         </DefaultLayout>
+        <div className="flex flex-col py-2">
+          <div className="flex flex-row justify-end gap-3" aria-disabled>
+            <div className="relative">
+              <TextInput
+                className="pointer-events-none max-w-36 cursor-default bg-white"
+                placeholder="Urutkan"
+                disabled
+              />
+              <img className="absolute right-2 top-[9px]" src="/assets/down.png" />
+            </div>
+            <div className="relative">
+              <TextInput placeholder="Cari Kelas" />
+              <img className="absolute right-3 top-[9px]" src="/assets/search.png" />
+            </div>
+          </div>
+          <div className="mt-5 box-border grid w-full grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-5 py-[20px_10px]">
+            {!isMobile
+              ? contents.map((item) => (
+                  <DefaultLayout className="rounded-md p-7">
+                    <div className="flex flex-col gap-1">
+                      <img
+                        className="mb-5 h-[200px] w-auto rounded-md object-cover"
+                        src={item.contentImage}
+                        alt={item.title}
+                      />
+                      <h3 className="text-heading5 font-bold">{item.title}</h3>
+                      <p className="text-bodyMedium">{item.description}</p>
+                      <div className="mt-5 flex items-center gap-5">
+                        <img className="h-10 w-10 rounded-sm" src={item.avatar} alt={item.name} />
+                        <div className="flex flex-col">
+                          <h4 className="text-heading6 font-bold">{item.name}</h4>
+                          <p className="text-bodyMedium font-medium">
+                            Senior accountant di <strong>Gojek</strong>
+                          </p>
+                        </div>
+                      </div>
+                      <div className="mt-5 flex flex-row items-center justify-between">
+                        <div className="flex flex-row items-center gap-2">
+                          <StarRatingUi rating={item.rating} size={20} />
+                          <p className="text-bodyMedium font-medium">
+                            {item.rating} ({item.reviewCount})
+                          </p>
+                        </div>
+                        <p className="text-heading4 font-bold text-[#3ECF4C]">{item.price}</p>
+                      </div>
+                    </div>
+                  </DefaultLayout>
+                ))
+              : contents.map((item) => (
+                  <DefaultLayout className="rounded-md p-4">
+                    <div className="flex flex-row gap-3">
+                      <img
+                        className="h-[86px] w-[85px] rounded-md object-cover"
+                        src={item.contentImage}
+                        alt={item.title}
+                      />
+                      <div className="flex flex-col justify-between">
+                        <h3 className="text-heading6 font-bold">{item.title}</h3>
+                        <div className="flex items-center gap-3">
+                          <img className="h-8 w-8 rounded-sm" src={item.avatar} alt={item.name} />
+                          <div className="flex flex-col">
+                            <h4 className="text-bodyMedium font-bold">{item.name}</h4>
+                            <p className="text-bodySmall font-medium">Senior accountant</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mt-2 flex flex-row items-center justify-between">
+                      <div className="flex flex-row items-center gap-2">
+                        <StarRatingUi rating={item.rating} size={20} />
+                        <p className="text-bodyMedium font-medium">
+                          {item.rating} ({item.reviewCount})
+                        </p>
+                      </div>
+                      <p className="text-heading4 font-bold text-[#3ECF4C]">{item.price}</p>
+                    </div>
+                  </DefaultLayout>
+                ))}
+          </div>
+        </div>
       </div>
       <FooterLayout />
     </>
